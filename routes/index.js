0a3f9083f5aad387083423389d61db209154b7aa
Roman Klinetskyi <iromaq@gmail.com>
2019-07-26 10:50:53 +0300

    merged and uploaded with last updates

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const pagesController = require('controllers/pages');
+const authController = require('controllers/auth');
+const placesController = require('controllers/places');
+
+router.get('/', function(req, res, next) {
+  res.redirect('/home');
+});
+
+router.get('/home', pagesController.dashboardView);
+
+router.get('/login', authController.loginView);
+router.post('/login', authController.loginAction);
+
+router.get('/register', authController.registerView);
+router.post('/register', authController.registerAction);
+
+router.get('/places', placesController.placesView);
+router.post('/getPlacesFromGoogle', placesController.getAddressesFromGooglePlaces);
+router.post('/setMyAddressFromGooglePlaces', placesController.setMyAddressFromGooglePlaces);
+
+router.get('/profile', pagesController.profileView);
+router.post('/profile', pagesController.profileAction);
+
+router.get('/password', function(req, res, next) {
+  res.render('password', { title: 'Change password' });
+});
+
+router.get('/admin', function(req, res, next) {
+  res.render('main');
+});
+
+router.get('/users', authController.onlyAdmin, pagesController.userView);
+
+router.get('/logout', authController.logout);
+
+module.exports = router;
